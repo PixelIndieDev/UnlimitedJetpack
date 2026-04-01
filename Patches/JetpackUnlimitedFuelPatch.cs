37665d9a33e4ca4295eb2c79a35e6e3cb250a7d8
Pixel <89741457+PixelIndieDev@users.noreply.github.com>
2026-04-01 14:31:09 +0200

    Add files via upload

---
+++ Patches/JetpackUnlimitedFuelPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+namespace UnlimitedJetpack.Patches
+{
+    [HarmonyPatch(typeof(GrabbableObject))]
+    internal class JetpackUnlimitedFuelPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void PatchJetpack(GrabbableObject __instance)
+        {
+            if (__instance is JetpackItem && __instance.IsOwner)
+            {
+                __instance.insertedBattery.charge = 1f;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnlimitedJetpack.Patches;
+
+namespace UnlimitedJetpack
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class JetpackFuelPatchBase : BaseUnityPlugin
+    {
+        private const string modGUID = "PixelIndieDev_UnlimitedJetpack";
+        private const string modName = "Unlimited Jetpack";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+        private static JetpackFuelPatchBase instance;
+
+        internal ManualLogSource logSource;
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+
+            logSource = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            harmony.PatchAll(typeof(JetpackFuelPatchBase));
+            harmony.PatchAll(typeof(JetpackUnlimitedFuelPatch));
+
+            logSource.LogInfo(modName + ": patch applied successfully");
+        }
+    }
+}
